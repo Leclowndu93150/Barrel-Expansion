5884fc3311715ffa3abcc1205e7d1fd2698697dd
Leclowndu93150 <email@>
2025-02-02 14:18:25 +0100

    tags don't apply but they generate

--- src/main/java/com/leclowndu93150/barrel_expansion/datagen/BarrelDataGenerator.java
+++ src/main/java/com/leclowndu93150/barrel_expansion/datagen/BarrelDataGenerator.java
@@ -1,9 +1,12 @@
 package com.leclowndu93150.barrel_expansion.datagen;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.leclowndu93150.barrel_expansion.BarrelExpansion;
 import com.leclowndu93150.barrel_expansion.events.BarrelEvents;
 import com.leclowndu93150.barrel_expansion.events.VanillaBarrels;
 import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
+import net.mehvahdjukaar.moonlight.api.resources.ResType;
 import net.mehvahdjukaar.moonlight.api.resources.SimpleTagBuilder;
 import net.mehvahdjukaar.moonlight.api.resources.pack.DynServerResourcesGenerator;
 import net.minecraft.advancements.Advancement;
@@ -19,16 +22,20 @@ import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.conditions.ICondition;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class BarrelDataGenerator extends DynServerResourcesGenerator {
-
     public static final BarrelDataGenerator INSTANCE = new BarrelDataGenerator();
+    private final List<String> blockValues = new ArrayList<>();
+    private final List<String> itemValues = new ArrayList<>();
 
     public BarrelDataGenerator() {
         super(new net.mehvahdjukaar.moonlight.api.resources.pack.DynamicDataPack(
@@ -40,21 +47,72 @@ public class BarrelDataGenerator extends DynServerResourcesGenerator {
         this.dynamicPack.setGenerateDebugResources(PlatHelper.isDev());
     }
 
+    private JsonObject createLootTable(ResourceLocation itemId) {
+        JsonObject root = new JsonObject();
+        root.addProperty("type", "minecraft:block");
+
+        JsonArray pools = new JsonArray();
+        JsonObject pool = new JsonObject();
+        pool.addProperty("rolls", 1);
+        pool.addProperty("bonus_rolls", 0);
+
+        JsonArray conditions = new JsonArray();
+        JsonObject survives = new JsonObject();
+        survives.addProperty("condition", "minecraft:survives_explosion");
+        conditions.add(survives);
+        pool.add("conditions", conditions);
+
+        JsonArray entries = new JsonArray();
+        JsonObject entry = new JsonObject();
+        entry.addProperty("type", "minecraft:item");
+        entry.addProperty("name", itemId.toString());
+
+        JsonArray functions = new JsonArray();
+        JsonObject copyName = new JsonObject();
+        copyName.addProperty("function", "minecraft:copy_components");
+        copyName.addProperty("source", "block_entity");
+        JsonArray include = new JsonArray();
+        include.add("minecraft:custom_name");
+        copyName.add("include", include);
+        functions.add(copyName);
+
+        entry.add("functions", functions);
+        entries.add(entry);
+        pool.add("entries", entries);
+        pools.add(pool);
+        root.add("pools", pools);
+        root.addProperty("random_sequence", "minecraft:blocks/" + itemId.getPath());
+
+        return root;
+    }
+
     @Override
     public void regenerateDynamicAssets(ResourceManager manager) {
+        blockValues.clear();
+        itemValues.clear();
+
         Stream.concat(
                 BarrelEvents.REGISTERED_BARRELS.values().stream(),
                 VanillaBarrels.VANILLA_BARRELS.values().stream()
         ).forEach(barrel -> {
             String barrelId = barrel.woodType() + "_barrel";
             Item barrelItem = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath(BarrelExpansion.MODID, barrelId));
+            Block barrelBlock = BuiltInRegistries.BLOCK.get(ResourceLocation.fromNamespaceAndPath(BarrelExpansion.MODID, barrelId));
 
-            // Generate tags
-            SimpleTagBuilder woodenBarrels = SimpleTagBuilder.of(ResourceLocation.fromNamespaceAndPath("c", "wooden_barrels"));
+            SimpleTagBuilder mineableAxe = SimpleTagBuilder.of(ResourceLocation.fromNamespaceAndPath("minecraft", "mineable/axe"));
+            mineableAxe.addEntry(barrelBlock);
+            dynamicPack.addTag(mineableAxe, Registries.BLOCK);
+
+            SimpleTagBuilder woodenBarrels = SimpleTagBuilder.of(ResourceLocation.fromNamespaceAndPath("c", "barrels"));
             woodenBarrels.addEntry(barrelItem);
             dynamicPack.addTag(woodenBarrels, Registries.ITEM);
 
-            // Generate crafting recipe
+            blockValues.add(BuiltInRegistries.BLOCK.getKey(barrelBlock).toString());
+            itemValues.add(BuiltInRegistries.ITEM.getKey(barrelItem).toString());
+
+            JsonObject lootTable = createLootTable(BuiltInRegistries.ITEM.getKey(barrelItem));
+            dynamicPack.addJson(ResourceLocation.fromNamespaceAndPath(BarrelExpansion.MODID, "blocks/" + barrelId), lootTable, ResType.LOOT_TABLES);
+
             Item planks = BuiltInRegistries.ITEM.get(barrel.planksId());
             Item slabs = BuiltInRegistries.ITEM.get(ResourceLocation.fromNamespaceAndPath(barrel.planksId().getNamespace(),
                     barrel.woodType() + "_slab"));
